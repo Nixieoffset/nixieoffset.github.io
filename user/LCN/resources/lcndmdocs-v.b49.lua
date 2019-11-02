ee8fe6bc58b460493fbe8170b329202f588d2f6d
Nixieoffset <34421248+Nixieoffset@users.noreply.github.com>
2019-11-02 16:35:52 +0200

    Create lcndmdocs-v.b49.lua

---
+++ user/LCN/resources/lcndmdocs-v.b49.lua
@@ -0,0 +1,31 @@
+<head>
+<style>
+body{background-color:#000022;width:70%;margin:auto;font-family:"Verdana Pro",Verdana,Consolas,monospace;}
+h1, h2, h3, h4, h5, h6 {text-align:center;color:#ED67EA;}  
+li {color:#9292aa;font-size:12px;}
+acc-purp {color:#8a2be2;}
+acc-grbl {color:#b3b3ff;}
+acc-lbrp {color:#b3b3ff;}
+acc-brp {color:#6e6eff;}
+acc-lbr2 {color:#8383cc;font-size:10px;}
+</style>
+</head>
+<br>
+<h1>#LCNdeathmatch v.b49 dokumentācija</h1>
+<h3>APZĪMĒJUMI</h3>
+<ul>
+<li><acc-brp>[obligāts parametrs]</acc-brp></li>
+<li><acc-grbl>[neobligāts parametrs]</acc-brp></li>
+</ul>
+<br>
+<h3>KOMANDAS</h3>
+<ul>
+<li>!d <acc-brp>[vārds]</acc-brp><i> - diskvalificē spēlētāju (piezīme: #LCN ekvivalents !d*)</i></li>
+<li>!end <i> - parāda beigu ekrānu (piezīme: #LCN ekvivalents !end)</i></li>
+<li>!limit <acc-lbrp>[milisekundes]</acc-lbrp><i> - maina noildzes laiku, kurā nevar šaut, noklusējuma vērtība 1000 (1 sekunde)</i></li>
+<li>!sp <acc-brp>[vārds]</acc-brp><i> - padara spēlētāju par skatītāju (piezīme: #LCN ekvivalents !sp)</i></li>
+<li>!type <acc-lbrp>[objekta ID]</acc-lbrp><i> - maina lodes uz citiem objektiem</i>
+     ╰ pieejamie ID, kas darbojas normāli: <i>17<acc-lbr2>(00)</acc-lbr2> - lode, 24 - eksplozija, 85<acc-lbr2>(00)</acc-lbr2> - laukakmens</i>
+</li>
+</ul>
+<acc-lbr2>PIEZĪME: Komandas var izmantot tikai moduļa palaidējs!</acc-lbr2>
